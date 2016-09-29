40ca91af041769c6f84849e78e9979f6f0a9d11b
Tony Tian <tony.tian@adscale.co.nz>
2016-09-29 13:35:49 +1300

    Update maven dependencies

---
+++ bService/src/main/scala/bragi/BotRunner.scala
@@ -0,0 +1,15 @@
+package bragi
+
+import com.flyberrycapital.slack.SlackClient
+
+object BotRunner {
+
+
+
+    def main(args: Array[String]) {
+//        val s = new SlackClient()
+
+        println("Slack bot runner started!")
+    }
+
+}
